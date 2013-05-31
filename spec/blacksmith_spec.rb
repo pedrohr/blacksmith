0be78c27250e1f198c5bfc2303a17bb102eb9e10
Pedro Assis <pedroh2306@gmail.com>
2013-05-31 02:51:06 -0300

    Extracting subsentences according to window sizes

--- spec/blacksmith_spec.rb
+++ spec/blacksmith_spec.rb
@@ -20,5 +20,23 @@ class BlacksmitTest < Test::Unit::TestCase
     assert_equal(@blacksmith.sentences, @sentences)
   end
 
-  
+  def test_should_isolate_ids_and_pure_text
+    assert_equal(@blacksmith.isolate_ids_and_pure_text('</Anarchism> is often defined as a </Political_philosophy> which holds the state to be undesirable, unnecessary, or harmful'), ['</Anarchism>', 'is often defined as a', '</Political_philosophy>', 'which holds the state to be undesirable, unnecessary, or harmful'])
+    assert_equal(@blacksmith.isolate_ids_and_pure_text('</Anarchism> as a mass </Social_movement> has regularly endured fluctuations in popularity'), ['</Anarchism>', 'as a mass', '</Social_movement>', 'has regularly endured fluctuations in popularity'])
+    assert_equal(@blacksmith.isolate_ids_and_pure_text("The so-called-</Anarchism>'s father is </John>"), ["The so-called-", "</Anarchism>", "'s father is", "</John>"])
+  end
+
+  def test_should_extract_windows
+    assert_equal(@blacksmith.extract_windows(@sentences[0][0], 0), ["", "is often defined as a", ""])
+    assert_equal(@blacksmith.extract_windows(@sentences[1][0], 0), ["", "as a mass", ""])
+    assert_equal(@blacksmith.extract_windows(@sentences[2][0], 0), ["", "as a", ""])
+
+    assert_equal(@blacksmith.extract_windows(@sentences[0][0], 1), ["", "is often defined as a", "which"])
+    assert_equal(@blacksmith.extract_windows(@sentences[1][0], 1), ["", "as a mass", "has"])
+    assert_equal(@blacksmith.extract_windows(@sentences[2][0], 1), ["of", "as a", "has"])
+
+    assert_equal(@blacksmith.extract_windows(@sentences[0][0], 2), ["", "is often defined as a", "which holds"])
+    assert_equal(@blacksmith.extract_windows(@sentences[1][0], 2), ["", "as a mass", "has regularly"])
+    assert_equal(@blacksmith.extract_windows(@sentences[2][0], 2), ["tendency of", "as a", "has been"])
+  end
 end
--- src/blacksmith.rb
+++ src/blacksmith.rb
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+require 'pp'
 class Blacksmith
   attr_reader :dbpedia_info, :sentences
 
@@ -21,5 +22,89 @@ class Blacksmith
     @sentences = sentences
   end
 
-  
+  def isolate_ids_and_pure_text(sentence)
+    vec = []
+    to_push = true
+
+    return [""] if sentence.empty?
+
+    sentence.split(" ").each do |token|
+      if (token.include?("</"))
+        
+        left = token.match(/(.+)<.+/)
+        unless left.nil?
+          if (to_push)
+            vec.push(left[1])
+          else
+            vec[vec.size-1] = vec.last + " " + left[1]
+          end
+        end
+
+        vec.push(token.match(/(<.+>)/)[1])
+        to_push = true
+
+        right = token.match(/.+>(.+)/)
+        unless right.nil?
+          vec.push(right[1])
+          to_push = false
+        end
+      else
+        if (to_push)
+          vec.push(token)
+          to_push = false
+        else
+          vec[vec.size-1] = vec.last + " " + token
+        end
+      end
+    end
+
+    return vec
+  end
+
+  def extract_windows(sentence, k)
+    tokens = isolate_ids_and_pure_text(sentence)
+    
+    left_window = ""
+    inside_window = ""
+    right_window = ""
+    
+    first_entity = false
+    second_entity = false
+    
+    tokens.each do |token|
+      if token.include?("</")
+        unless first_entity
+          first_entity = true
+        else
+          second_entity = true
+        end
+      elsif (first_entity)
+        if (second_entity)
+          right_window += token
+        else
+          inside_window += token
+        end
+      else
+        left_window += token
+      end
+    end
+
+    left = ""
+    if k > 0
+      left_window = left_window.split(" ")
+      k.times do |i|
+        left = left_window[left_window.size - 1 - i] + " " + left unless left_window[left_window.size - 1 - i].nil?
+      end
+    end
+
+    right = ""
+    if k > 0
+      right_window = right_window.split(" ")
+      k.times do |i|
+        right += right_window[i] + " " unless right_window[i].nil?
+      end
+    end
+
+    return [left.strip, inside_window.strip, right.strip]
+  end
 end
